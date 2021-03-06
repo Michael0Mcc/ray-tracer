bc5982c76a395fb65649c39943b9a693e8c273e2
Michael McCarthy <thmichael300@gmail.com>
2021-03-06 17:36:54 -0600

    uses new system to write color

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,10 @@ pub fn printimage(imagewidth: usize, imageheight: usize) {
 	for j in (0..imageheight).rev() {
 		eprintln!("\rScanlines remaining: {}", j);
 		for i in 0..imagewidth {
-			let r: f64 = (i as f64) / ((imagewidth-1) as f64);
-			let g: f64 = (j as f64) / ((imageheight-1) as f64);
-			let b: f64 = 0.25;
+			let pixel_color: Color = Color((i as f64) / ((imagewidth-1) as f64), (j as f64) / ((imageheight-1) as f64), 0.25, 1.0);
 
 			// print!("{} {} {} \n", (r*255.999) as i32, (g*255.999) as i32,(b*255.999) as i32);
-			write_color(Color(r, g, b, 1.0));
+			write_color(pixel_color);
 		}
 	}
 }
